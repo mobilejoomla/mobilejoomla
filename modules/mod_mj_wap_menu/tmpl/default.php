9794201969d0b5d3beb375676b4f811381796b5b
bmllnet <bmllnet@8ac64075-9078-0410-8966-e09458e102b6>
2010-03-10 20:34:35 +0000

    @D: clear denis branch
    git-svn-id: svn+ssh://64.131.77.225/home/bmllnet/svn/branches/denis@294 8ac64075-9078-0410-8966-e09458e102b6

--- modules/mod_mj_wap_menu/tmpl/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// no direct access
-defined('_JEXEC') or die('Restricted access');
-
-mosShowWAPMenu($params);
--- modules/mod_mj_wap_search/tmpl/default.php
+++
@@ -1,8 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<p>
-<input type="text" name="searchword" maxlength="<?php echo $maxlength; ?>" value="<?php echo $text; ?>" />
-</p>
-<do type="accept" title="<?php echo $button_text; ?>">
-<go href="index.php?option=com_search&amp;searchword=$(searchword)" />
-</do>
