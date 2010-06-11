5e9379a6e954b1c555e550891d1c5f4f221794f0
bmllnet <bmllnet@8ac64075-9078-0410-8966-e09458e102b6>
2010-06-11 08:36:09 +0000

    @D: clear 0.9.4
    git-svn-id: svn+ssh://64.131.77.225/home/bmllnet/svn/branches/denis@340 8ac64075-9078-0410-8966-e09458e102b6

--- templates/mobile_wap/html/com_content/frontpage/default_links.php
+++
@@ -1,6 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<strong><?php echo JText::_( 'More Articles...' ); ?></strong><br />
-<?php foreach ($this->links as $link) : ?>
-<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $link->title; ?></a><br />
-<?php endforeach; ?>
