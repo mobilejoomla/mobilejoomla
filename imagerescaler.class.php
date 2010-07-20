1695338c2b0e999155a1387a091f2aa945431c43
bmllnet <bmllnet@8ac64075-9078-0410-8966-e09458e102b6>
2010-07-20 10:29:04 +0000

    @D: fix of imagerescaling to work in subdirectory
    git-svn-id: svn+ssh://64.131.77.225/home/bmllnet/svn/branches/denis@360 8ac64075-9078-0410-8966-e09458e102b6

--- imagerescaler.class.php
+++ imagerescaler.class.php
@@ -72,8 +72,7 @@ class ImageRescaler
 
 		if(strpos($imageurl, '://') == false)
 		{
-			if($imageurl
-			{0}!=='/')
+			if($imageurl{0}!=='/')
 				$imageurl = '/'.$imageurl;
 			$src_imagepath = JPATH_SITE.$imageurl;
 		}
@@ -144,7 +143,7 @@ class ImageRescaler
 
 		$dest_imagedir = dirname($src_imagepath).DS.ImageRescaler::$thumbdir;
 		$dest_imagepath = $dest_imagedir.DS.$src_imagename.'_'.$dest_width.'x'.$dest_height.'.'.$dest_ext;
-		$dest_imageuri = implode('/', explode(DS, substr($dest_imagepath, strlen(JPATH_SITE))));
+		$dest_imageuri = JURI::base(true).'/'.implode('/', explode(DS, substr($dest_imagepath, strlen(JPATH_SITE))));
 		if(file_exists($dest_imagepath))
 			return $dest_imageuri;
 
