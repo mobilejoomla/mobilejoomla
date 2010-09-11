3be9caab55de3d161179c0bbcd3fe866ec1fd9ba
bmllnet <bmllnet@8ac64075-9078-0410-8966-e09458e102b6>
2010-09-11 15:02:11 +0000

    Trying to setup source folder with dummy setup.py
    git-svn-id: svn+ssh://64.131.77.225/home/bmllnet/svn/branches/denis@428 8ac64075-9078-0410-8966-e09458e102b6

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+
+    This is a dummy setup.py file which causes this source code tree
+    to be Python egg compatible. We can use automated source code
+    management tools better when this file is present-
+    
+    http://github.com/fschulze/mr.developer/issues/issue/27
+
+"""
+
+from distutils.core import setup
+
+setup(name='mobilejoomla',
+      description='Mobilejoomla extension for Joomla!',
+      author='Mobilejooma team',
+      author_email='mikko@kuneri.net',
+      url='http://www.mobilejoomla.com',
+      packages=[],
+      version="0.0"
+     )
