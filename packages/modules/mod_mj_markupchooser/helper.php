c7d9546b663d05e0f88849e8dda03312e4762fe8
Mobile Joomla! <denis@kuneri.net>
2011-02-22 00:16:22 +0300

    fix mod_mj_markupchooser in joomla1.6

--- packages/modules/mod_mj_markupchooser/helper.php
+++ packages/modules/mod_mj_markupchooser/helper.php
@@ -12,27 +12,27 @@ defined('_JEXEC') or die('Restricted access');
 
 class modMarkupChooserHelper
 {
-        
+
 	var $base;
 	var $return;
 
 	var $show_chosen_markup;
 
-        
-        function modMarkupChooserHelper($base, $return, $show_chosen_markup) {
-                $this->base = $base;     
-                $this->return = $return;
-                $this->show_chosen_markup = $show_chosen_markup;
-        }
+	function modMarkupChooserHelper($base, $return, $show_chosen_markup)
+	{
+		$this->base = $base;
+		$this->return = $return;
+		$this->show_chosen_markup = $show_chosen_markup;
+	}
 
-        /**
-         * No idea what this chould do, but it was definitely broken by how it used static variables and did not run on PHP 5.3 -mikko
-         */
+	/**
+	* No idea what this chould do, but it was definitely broken by how it used static variables and did not run on PHP 5.3 -mikko
+	*/
 	function getChangeLink($user_markup, $test_markup)
 	{
 		if($user_markup == $test_markup)
 			return $this->show_chosen_markup?'':false;
 		else
-			return $this->base.'index2.php?option=com_mobilejoomla&amp;task=setmarkup&amp;markup='.$test_markup.'&amp;return='.$this->return;
+			return $this->base.'index.php?option=com_mobilejoomla&amp;task=setmarkup&amp;markup='.$test_markup.'&amp;return='.$this->return;
 	}
 }
