9ccd44ad3075f751fffd8c6acd522cf4efc68636
Mobile Joomla! <denis@kuneri.net>
2011-02-22 11:56:57 +0300

    fix EntityRef issue

--- packages/templates/mobile_iphone/html/com_content/section/blog_links.php
+++ packages/templates/mobile_iphone/html/com_content/section/blog_links.php
@@ -3,6 +3,6 @@
 <ul>
 <?php
  foreach ($this->links as $link) : ?>
-<li><a class="blogsection" href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $link->title; ?></a></li>
+<li><a class="blogsection" href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $this->escape($link->title); ?></a></li>
 <?php endforeach; ?>
 </ul>
--- packages/templates/mobile_pda/html/com_content/category/blog_links.php
+++ packages/templates/mobile_pda/html/com_content/category/blog_links.php
@@ -3,6 +3,6 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <div><strong><?php echo JText::_( 'More Articles...' ); ?></strong></div>
 <ul>
 <?php foreach ($this->links as $link) : ?>
-<li><a class="blogsection" href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $link->title; ?></a></li>
+<li><a class="blogsection" href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $this->escape($link->title); ?></a></li>
 <?php endforeach; ?>
 </ul>
