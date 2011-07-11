2c5e5450e4a7dcdc5c84890ad4564d983c60f706
Mobile Joomla! <denis@kuneri.net>
2011-07-11 23:54:31 +0400

    Fix adminicon module for Joomla!1.7

--- packages/modules/mod_mj_adminicon/mod_mj_adminicon.php
+++ packages/modules/mod_mj_adminicon/mod_mj_adminicon.php
@@ -14,10 +14,15 @@ $document =& JFactory::getDocument();
 $lang =& JFactory::getLanguage();
 $version = new JVersion();
 
-$document->addStyleSheet(JURI::base().'modules/mod_mj_adminicon/css/mod_mj_adminicon.css');
+$document->addStyleSheet('modules/mod_mj_adminicon/css/mod_mj_adminicon.css');
 $lang->load('com_mobilejoomla', JPATH_ADMINISTRATOR);
 
-$is_joomla15 = (substr($version->getShortVersion(),0,3) == '1.5');
+switch(substr($version->getShortVersion(),0,3))
+{
+case '1.5': $iconclass = 'icon15'; break;
+case '1.6': $iconclass = 'icon16'; break;
+case '1.7': $iconclass = 'icon17'; break;
+}
 
 include_once JPATH_ADMINISTRATOR.DS.'components'.DS.'com_mobilejoomla'.DS.'admin.mobilejoomla.html.php';
 HTML_MobileJoomla::CheckForUpdate();
@@ -25,16 +30,16 @@ HTML_MobileJoomla::CheckForUpdate();
 JHTML::_('behavior.modal', 'a.modal');
 ?>
 <div id="mjicon">
-	<div id="mjnoupdate" style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
-		<div class="icon <?php echo $is_joomla15 ? 'icon15' : 'icon16'; ?>">
+	<div id="mjnoupdate" class="icon-wrapper" style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
+		<div class="icon <?php echo $iconclass; ?>">
 			<a href="index.php?option=com_mobilejoomla">
 				<img src="modules/mod_mj_adminicon/images/mj-cpanel.png" />
 				<span><?php echo JText::_('COM_MJ__MOBILEJOOMLA'); ?></span>
 			</a>
 		</div>
 	</div>
-	<div id="mjupdate" style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
-		<div class="icon <?php echo $is_joomla15 ? 'icon15' : 'icon16'; ?>">
+	<div id="mjupdate" class="icon-wrapper" style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
+		<div class="icon <?php echo $iconclass; ?>">
 			<a class="modal" href="index.php?tmpl=component&option=com_mobilejoomla&task=update" rel="{handler: 'iframe', size: {x: 480, y: 320}}">
 				<img src="modules/mod_mj_adminicon/images/mj-update.png" />
 				<span><?php echo JText::_('COM_MJ__UPDATE_AVAILABLE'); ?></span>
