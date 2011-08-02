fed6ddf7a8209b285dd285f5f7bd1a9949e749f9
Mobile Joomla! <denis@kuneri.net>
2011-08-02 16:50:54 +0400

    old-style pagination for mobile_iphone

--- packages/templates16/mobile_iphone/html/pagination.php
+++ packages/templates16/mobile_iphone/html/pagination.php
@@ -13,10 +13,10 @@ defined('_JEXEC') or die('Restricted access');
 function pagination_list_footer($list)
 {
 	$html = '<div class="list-footer">';
-	$html .= "\n".'<div class="limit">'.JText::_('Display Num').$list['limitfield'].'</div>';
+	$html .= "\n".'<div class="limit">'.JText::_('JGLOBAL_DISPLAY_NUM').$list['limitfield'].'</div>';
 	$html .= $list['pageslinks'];
 	$html .= "\n".'<div class="counter">'.$list['pagescounter'].'</div>';
-	$html .= "\n".'<input type="hidden" name="limitstart" value="'.$list['limitstart'].'" />';
+	$html .= "\n".'<input type="hidden" name="'.$list['prefix'].'limitstart" value="'.$list['limitstart'].'" />';
 	$html .= "\n".'</div>';
 	return $html;
 }
