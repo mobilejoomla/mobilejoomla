76f6a6777dd4fff7385dd45d0afdf00b978443ee
Mobile Joomla! <denis@kuneri.net>
2011-08-06 23:55:55 +0400

    fix redirect after login issue in mobile_iphone

--- packages/templates15/mobile_iphone/html/com_user/login/default_login.php
+++ packages/templates15/mobile_iphone/html/com_user/login/default_login.php
@@ -72,7 +72,7 @@ endif; ?>
 
 	<input type="hidden" name="option" value="com_user" />
 	<input type="hidden" name="task" value="login" />
-	<input type="hidden" name="return" value="<?php echo $return; ?>" />
+	<input type="hidden" name="return" value="<?php echo $this->return; ?>" />
 	<?php echo JHTML::_( 'form.token' ); ?>
 </form>
 
