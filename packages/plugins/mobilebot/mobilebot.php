4d1ccf3a1734085f6250ef090a32f78ef644f9f4
Mobile Joomla! <denis@kuneri.net>
2011-08-15 11:06:58 +0400

    misspel.

--- packages/plugins/mobilebot/mobilebot.php
+++ packages/plugins/mobilebot/mobilebot.php
@@ -390,7 +390,7 @@ class plgSystemMobileBot extends JPlugin
 		unset($current['no_html']);
 		unset($current['language']);
 		unset($current['tp']);
-		unset($current['template');
+		unset($current['template']);
 		unset($current['templateStyle']);
 		unset($current['limit']); // fix for sh404sef
 		if(isset($current[session_name()]))
