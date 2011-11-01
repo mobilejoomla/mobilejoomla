e622d98c59191d1454c94232f71362cee6c27f20
Mobile Joomla! <denis@kuneri.net>
2011-11-01 20:27:40 +0400

    fix typing error

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ MJ_URL="http://www.mobilejoomla.com"
 MJ_DESC="Mobile Joomla!"
 MJ_AUTHOR="Mobile Joomla!"
 MJ_EMAIL="hello@mobilejoomla.com"
-MJ_COPYRIGTH="(C) 2008-$CUR_YEAR Mobile Joomla!"
+MJ_COPYRIGHT="(C) 2008-$CUR_YEAR Mobile Joomla!"
 MJ_LICENSE="http://www.gnu.org/licenses/gpl-2.0.htm GNU/GPL"
 
 # do replacements
@@ -18,6 +18,6 @@ do
 	then
 		echo "Processing $F ..."
 		# assume that gnu sed is installed, so use -i option
-		sed -i -e "s|###VERSION###|$MJ_VERSION|g" -e "s|###DATE###|$MJ_DATE|g" -e "s|###URL###|$MJ_URL|g" -e "s|###DESC###|$MJ_DESC|g" -e "s|###AUTHOR###|$MJ_AUTHOR|g" -e "s|###EMAIL###|$MJ_EMAIL|g" -e "s|###COPYRIGHT###|$MJ_COPYRIGTH|g" -e "s|###LICENSE###|$MJ_LICENSE|g" $F
+		sed -i -e "s|###VERSION###|$MJ_VERSION|g" -e "s|###DATE###|$MJ_DATE|g" -e "s|###URL###|$MJ_URL|g" -e "s|###DESC###|$MJ_DESC|g" -e "s|###AUTHOR###|$MJ_AUTHOR|g" -e "s|###EMAIL###|$MJ_EMAIL|g" -e "s|###COPYRIGHT###|$MJ_COPYRIGHT|g" -e "s|###LICENSE###|$MJ_LICENSE|g" $F
 	fi
 done
