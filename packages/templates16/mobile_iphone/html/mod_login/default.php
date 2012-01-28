53c29f6398a6ad3e129d0aa2792ee8fcd76188a0
Mobile Joomla! <denis@kuneri.net>
2012-01-29 02:18:26 +0400

    mod_login and mod_search for mobile_iphone in J!1.6

---
+++ packages/templates16/mobile_iphone/html/mod_login/default.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * ###DESC###
+ * ###URL###
+ *
+ * @version		###VERSION###
+ * @license		###LICENSE###
+ * @copyright	###COPYRIGHT###
+ * @date		###DATE###
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+JHtml::_('behavior.keepalive');
+?>
+<?php if ($type == 'logout') : ?>
+
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" name="login" id="form-login">
+<?php if ($params->get('greeting')) : ?>
+	<ul>
+	<li>
+	<?php if ($params->get('name')) : {
+		echo JText::sprintf( 'MOD_LOGIN_HINAME', $user->get('name') );
+	} else : {
+		echo JText::sprintf( 'MOD_LOGIN_HINAME', $user->get('username') );
+	} endif; ?>
+	</li>
+	</ul>
+<?php endif; ?>
+	<div class="loginButtonWrapper">
+		<input type="submit" name="Submit" class="button whiteButton loginButton" value="<?php echo JText::_( 'JLOGOUT'); ?>" />
+	</div>
+	<input type="hidden" name="option" value="com_users" />
+	<input type="hidden" name="task" value="user.logout" />
+	<input type="hidden" name="return" value="<?php echo $return; ?>" />
+</form>
+
+<?php else : ?>
+
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" name="login" id="form-login" >
+	<ul>
+	<?php echo $params->get('pretext'); ?>
+	<li id="form-login-username">
+		<!--<label for="modlgn_username"><?php echo JText::_('MOD_LOGIN_VALUE_USERNAME') ?></label>-->
+		<input id="modlgn_username" type="text" name="username" class="inputbox" alt="username" size="18"
+			placeholder="<?php echo JText::_('MOD_LOGIN_VALUE_USERNAME') ?>" value="" />
+	</li>
+	<li id="form-login-password">
+		<!--<label for="modlgn_passwd"><?php echo JText::_('JGLOBAL_PASSWORD') ?></label>-->
+		<input id="modlgn_passwd" type="password" name="password" class="inputbox" size="18" alt="password"
+			placeholder="<?php echo JText::_('JGLOBAL_PASSWORD') ?>" value ="" />
+	</li>
+	<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
+	<li id="form-login-remember">
+		<label for="modlgn_remember"><?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?></label>
+		<span class="toggle">
+			<input id="modlgn_remember" type="checkbox" name="remember" class="inputbox" value="yes" alt="Remember Me" />
+		</span>
+	</li> 
+	<?php endif; ?>
+	</ul>
+	<div class="loginButtonWrapper">
+		<input type="submit" name="Submit" class="button whiteButton loginButton" value="<?php echo JText::_('JLOGIN') ?>" />
+	</div>
+	<ul>
+		<li>
+			<a href="<?php echo JRoute::_( 'index.php?option=com_users&view=reset' ); ?>" class="mainlevel">
+			<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
+		</li>
+		<li>
+			<a href="<?php echo JRoute::_( 'index.php?option=com_users&view=remind' ); ?>" class="mainlevel">
+			<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
+		</li>
+		<?php
+		$usersConfig = &JComponentHelper::getParams( 'com_users' );
+		if ($usersConfig->get('allowUserRegistration')) : ?>
+		<li>
+			<a href="<?php echo JRoute::_( 'index.php?option=com_users&view=registration' ); ?>" class="mainlevel">
+				<?php echo JText::_('MOD_LOGIN_REGISTER'); ?></a>
+		</li>
+		<?php endif; ?>
+	</ul>
+	<?php echo $params->get('posttext'); ?>
+	<input type="hidden" name="option" value="com_user" />
+	<input type="hidden" name="task" value="login" />
+	<input type="hidden" name="return" value="<?php echo $return; ?>" />
+	<?php echo JHTML::_( 'form.token' ); ?>
+</form>
+
+<?php endif; ?>
---
+++ packages/templates16/mobile_iphone/html/mod_search/default.php
@@ -0,0 +1,34 @@
+<?php
+// no direct access
+defined('_JEXEC') or die;
+?>
+<form action="<?php echo JRoute::_('index.php');?>" method="post">
+<div class="search<?php echo $moduleclass_sfx;?>">
+<ul class="toolbar edgetoedge grayToolbar">
+<li>
+<?php
+	$output = '<input name="searchword" maxlength="'.$maxlength.'" class="inputbox'.$moduleclass_sfx.'" type="text" size="'.$width.'" placeholder="'.$text.'" value="'.($text==$label?'':$text).'" />';
+	$button = '<input type="submit" value="'.$button_text.'" class="button'.$moduleclass_sfx.'" />';
+
+	switch ($button_pos) :
+	    case 'none' :
+		    $output = '<table><tr><td class="text">'.$output.'</td></tr></table>';
+		    break;
+	    case 'right' :
+		    $output = '<table><tr><td class="text">'.$output.'</td><td class="button">'.$button.'</td></tr></table>';
+		    break;
+	    case 'left' :
+	    default :
+		    $output = '<table><tr><td class="button">'.$button.'</td><td class="text">'.$output.'</td></tr></table>';
+		    break;
+	endswitch;
+
+	echo $output;
+?>
+</li>
+</ul>
+</div>
+<input type="hidden" name="task"   value="search" />
+<input type="hidden" name="option" value="com_search" />
+<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>" />
+</form> 
