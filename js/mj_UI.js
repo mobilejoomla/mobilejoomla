bf4db25a72048e11c31dbd0df6646f0adb95d9f1
Mobile Joomla! <denis@kuneri.net>
2012-02-22 23:51:46 +0400

    correct version check for update

--- js/mj_UI.js
+++ js/mj_UI.js
@@ -1,4 +1,21 @@
 /* ###SHORTHEADER### */
+function version_compare(v1, v2){
+    var vm = {'dev': -4, 'alpha': -3, 'beta': -2, 'rc': -1},
+        vprep = function(v){return ('' + v).toLowerCase().replace(/([^.\d]+)/g, '.$1.').replace(/\s+/g, '').replace(/\.{2,}/g, '.').split('.');},
+        vnum = function(v){return !v ? 0 : (isNaN(v) ? vm[v] || -5 : parseInt(v, 10));};
+    v1 = vprep(v1);
+    v2 = vprep(v2);
+	var i = 0,
+		x = Math.max(v1.length, v2.length);
+    for(; i < x; i++){
+        if(v1[i]==v2[i]) continue;
+        v1[i] = vnum(v1[i]);
+        v2[i] = vnum(v2[i]);
+        return (v1[i] < v2[i]) ? 1 : -1;
+    }
+	return 0;
+}
+
 window.addEvent('domready',function(){
 	function hidetabs(){
 		$$('#submenu li a').each(function(a){
@@ -43,7 +60,7 @@ window.addEvent('domready',function(){
 				method: 'get',
 				update: 'mjlatestver',
 				onSuccess : function(response){
-					if(response != '0.9'){
+					if(version_compare('###VERSION###', response)>0){
 						$('mjlatestverurl').setStyle('display', 'block');
 					}
 				}
@@ -54,7 +71,7 @@ window.addEvent('domready',function(){
 				method: 'get',
 				update: $('mjlatestver'),
 				onComplete: function(response){
-					if(response != '0.9'){
+					if(version_compare('###VERSION###', response)>0){
 						$('mjlatestverurl').setStyle('display', 'block');
 					}
 				}
