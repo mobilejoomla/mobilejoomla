b43f4ccca87d52c0e0e8efdc5c9e95d528c432a1
Mobile Joomla! <denis@kuneri.net>
2012-02-23 02:19:08 +0400

    polish version_compare

--- js/mj_UI.js
+++ js/mj_UI.js
@@ -1,18 +1,18 @@
 /* ###SHORTHEADER### */
 function version_compare(v1, v2){
-    var vm = {'dev': -4, 'alpha': -3, 'beta': -2, 'rc': -1},
-        vprep = function(v){return ('' + v).toLowerCase().replace(/([^.\d]+)/g, '.$1.').replace(/\s+/g, '').replace(/\.{2,}/g, '.').split('.');},
-        vnum = function(v){return !v ? 0 : (isNaN(v) ? vm[v] || -5 : parseInt(v, 10));};
-    v1 = vprep(v1);
-    v2 = vprep(v2);
+	var vm = {'dev': -4, 'alpha': -3, 'beta': -2, 'rc': -1},
+		vprep = function(v){return ('' + v).toLowerCase().replace(/([^.\d]+)/g, '.$1.').replace(/\s+/g, '').replace(/\.{2,}/g, '.').split('.');},
+		vnum = function(v){return !v ? 0 : (isNaN(v) ? vm[v] || -5 : parseInt(v, 10));};
+	v1 = vprep(v1);
+	v2 = vprep(v2);
 	var i = 0,
 		x = Math.max(v1.length, v2.length);
-    for(; i < x; i++){
-        if(v1[i]==v2[i]) continue;
-        v1[i] = vnum(v1[i]);
-        v2[i] = vnum(v2[i]);
-        return (v1[i] < v2[i]) ? 1 : -1;
-    }
+	for(; i < x; i++){
+		if(v1[i]==v2[i]) continue;
+		v1[i] = vnum(v1[i]);
+		v2[i] = vnum(v2[i]);
+		return (v1[i] < v2[i]) ? 1 : -1;
+	}
 	return 0;
 }
 
