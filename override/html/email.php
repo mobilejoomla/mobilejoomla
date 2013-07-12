84b6a5351d5c5b08a25d5756bfea6b0f1de9a618
Mobile Joomla! <denis@kuneri.net>
2013-07-12 16:40:52 +0400

    fix to don't cloak twice

--- override/html/email.php
+++ override/html/email.php
@@ -14,6 +14,9 @@ abstract class JHtmlEmail
 {
 	public static function cloak($mail, $mailto = 1, $text = '', $email = 1)
 	{
+		if(strpos($mail, "'") !== false)
+			return $mail;
+
 		if($mailto)
 			$html = '<a href="javascript:void(location.href=\'mail\'+\'to:'.str_replace('@', "'+'@'+'", $mail).'\')">'.($text ? $text : str_replace('@', '(at)', $mail)).'</a>';
 		else
